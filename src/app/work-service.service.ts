51471678d385dbea674ddc95a4701e36ed824edf
FrankKamala <frankkamala@gmail.com>
2019-12-06 14:56:48 +0300

    add Token

--- src/app/work-service.service.ts
+++ src/app/work-service.service.ts
@@ -5,8 +5,10 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class WorkServiceService {
-
-
+  user: User[] = [];
+    _URL = 'https://api.github.com/users/';
+    token = '?access_token=d88ee8d7b4f4a19b94690ceea2231cb316dfe82e';
+//
 
   constructor(private http:HttpClient) {
     alert("Service is Ready");
