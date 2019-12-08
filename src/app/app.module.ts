04284a39a412b3d2fc2ef04a311ffea2c3af99b2
FrankKamala <frankkamala@gmail.com>
2019-12-08 20:16:55 +0300

    Added aboutpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http'; //import http
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
